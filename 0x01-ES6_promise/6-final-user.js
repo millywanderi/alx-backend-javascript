3ed7da21d6640fb368a4d75722ded452cc66a92a
millywanderi <wanderinyambura5@gmail.com>
2024-01-03 17:55:39 +0300

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const userP = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
-
-  return Promise.allSettled([userP, photo])
-    .then((results) => {
-      return results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }));
-    });
+  const data = await Promise.allSettled([userP, photo])
+    .then((results) => results.map((result) => {
+      if (result.status !== 'fulfilled') {
+        return {
+          status: 'rejected',
+          value: result.reason.toString(),
+        };
+      }
+      return result;
+    }));
+  return Promise.resolve(data);
 }
