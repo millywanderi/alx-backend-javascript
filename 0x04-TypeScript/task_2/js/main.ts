6321933e530e735c2fb8692d89a48edc74c4c9d7
millywanderi <wanderinyambura5@gmail.com>
2024-01-10 12:51:36 +0300

    Creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -50,3 +50,16 @@ function createEmployee(salary: number | string): Director | Teacher {
     return new Director();
   }
 }
+
+// Create functions specific to employees
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
