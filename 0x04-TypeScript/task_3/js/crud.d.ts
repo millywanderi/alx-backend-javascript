9d14171131b8875aa3f8ab24a04ebf21ac2ff8c1
millywanderi <wanderinyambura5@gmail.com>
2024-01-10 22:10:24 +0300

    Ambient Namespaces

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,3 @@
+export function insertRow(row:RowElement): RowID;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId: RowID, row: RowElement): RowID;
---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// Build an interface and a type
+export type RowId = number;
+
+// Interface RowElement that contains these 3 fields
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
