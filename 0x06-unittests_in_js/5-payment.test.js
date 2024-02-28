51f7b172d2f4c65de965bb434cb2afa12cd558c1
millywanderi <wanderinyambura5@gmail.com>
2024-02-29 00:34:09 +0300

    Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,37 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('calls sendPaymentRequestToApi with 100, and 20: and logs "The total is: 120" once', () => {
+    const totalAmount = 100;
+    const totalShipping = 20;
+    const expectedSum = Utilis.calculateNumber('SUM', totalAmount, totalShipping);
+
+     sendPaymentRequestToApi(totalAmount, totalShipping);
+
+     expect(consoleSpy.callOnce).to.be.true;
+     expect(consoleSpy.callWith(`The total is: ${expectedSum}`)).to.be.true;
+  });
+
+  it('calls sendPaymentRequestToApi with 10, and 10: and logs "The total is: 20" once', () => {
+    const totalAmount = 10;
+    const totalShipping = 10;
+    const expectedSum = Utilis.calculateNumber('SUM', totalAmount, totalShipping);
+
+    sendPaymentRequestToApi(totalAmount, totalShipping);
+
+    expect(consoleSpy.callOnce).to.be.true;
+    expect(consoleSpy.callWith(`The total is: ${expectedSum}`)).to.be.true;
+  });
+});
