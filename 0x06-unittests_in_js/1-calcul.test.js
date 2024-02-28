af1d2a4ddfd1dc6b3a45db4b59724ac1caac378a
millywanderi <wanderinyambura5@gmail.com>
2024-02-28 23:19:21 +0300

    Combining descriptions

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  it('handles adding tow rounded numbers when type is SUM', () => {
+    assert.equal(calculateNumber('SUM', 1.2, 3.7), 5);
+    assert.equal(calculateNumber('SUM', -1.2, -3.7), -5);
+  });
+
+  it('handles substracting two rounded numbers when type is SUBTRACT', () => {
+    assert.equal(calculateNumber('SUBTRACT', 5.9, 2.3), 4);
+    assert.equal(calculateNumber('SUBTRACT', -5.9, -2.3), -4);
+  });
+
+  it('handles dividing two rounded numbers when type is DIVIDE', () => {
+    assert.equal(calculateNumber('DIVIDE', 8, 2), 4);
+  });
+
+  it('returns Error when dividing by 0', () => {
+    assert.equal(calculateNumber('DIVIDE', 4, 0), 'Error');
+    assert.equal(calculateNumber('DIVIDE', -4, 0), 'Error');
+  });
+
+  it('throws an error when an invalid type is provided', () => {
+    assert.throws(() => calculateNumber('INVALID_TYPE', 1, 2), 'Error');
+  });
+});
